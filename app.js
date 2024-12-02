55520fe10523f00eafaca30092bba40bbee7d469
Muhammed Fathah <muhammedfathahofficial@gmail.com>
2024-12-02 21:54:47 +0530

    completed the wishlist product adding and that product move to the cart. but there is having some small bugs . out of stock products also add to the wishlist and that also move to the cart

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const auth = require("./middleware/authMiddleware")
 const productRoutes = require('./routes/productRoutes')
 const cartRoutes = require('./routes/cartRoutes')
 const orderRoutes = require('./routes/orderRoutes')
+const wishlistRoutes = require('./routes/wishlistRoutes')
 
 app.use(ejsLayouts);
 app.use(cookieParser())
@@ -44,6 +45,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); 
 
 app.use(auth.setCurrentRoute)
+app.use('/wishlist',wishlistRoutes)
 app.use(orderRoutes)
 app.use(cartRoutes)
 app.use("/admin",adminRoutes,categoryRoutes,productRoutes)
---
+++ controllers/wishlistController.js
@@ -0,0 +1,108 @@
+const Wishlist = require("../models/wishlist");
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+const addToWishlist = async (req, res) => {
+  try {
+    const { productId, productName, productPrice } = req.body;
+    const userId = req.session.user._id;
+
+    const existingItem = await Wishlist.findOne({ userId, productId });
+    if (existingItem) {
+      return res.status(400).json({ message: "Product already in wishlist" });
+    }
+
+    const newWishlistItem = new Wishlist({
+      userId,
+      productId,
+      productName,
+      productPrice,
+    });
+    await newWishlistItem.save();
+    res.status(201).json({ message: "Product added to wishlist" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error adding to wishlist" });
+  }
+};
+
+const getWishlist = async (req, res) => {
+  try {
+    const userId = req.session.user._id;
+    const wishlistItems = await Wishlist.find({ userId }).populate("productId");
+    res.render("my account/wishlist", { wishlistItems, layout: false });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching wishlist" });
+  }
+};
+
+const moveToCart = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    console.log("Received productId:", productId);
+    if (!req.session.user) {
+      return res.status(400).send("User not authenticated.");
+    }
+    const wishlistItem = await Wishlist.findById(productId);
+    console.log("Wishlist Item:", wishlistItem);
+
+    if (!wishlistItem) {
+      return res.status(404).send("Item not found in wishlist");
+    }
+
+    const product = await Product.findById(wishlistItem.productId);
+    console.log("Product:", product);
+
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+
+    const cartItem = {
+      productId: product._id,
+      quantity: 1,
+      deliveryDate: "2024-12-10",
+    };
+
+    let cart = await Cart.findOne({ user: req.session.user._id });
+    console.log("Found cart:", cart);
+
+    if (!cart) {
+      cart = new Cart({
+        user: req.session.user._id,
+        items: [cartItem],
+        totalPrice: product.discount_price * cartItem.quantity,
+      });
+    } else {
+      const existingItemIndex = cart.items.findIndex(
+        (item) => item.productId.toString() === product._id.toString()
+      );
+
+      if (existingItemIndex !== -1) {
+        const existingItem = cart.items[existingItemIndex];
+        existingItem.quantity += cartItem.quantity;
+      } else {
+        cart.items.push(cartItem);
+      }
+
+      cart.totalPrice = cart.items.reduce((total, item) => {
+        return total + item.quantity * product.discount_price;
+      }, 0);
+    }
+
+    if (isNaN(cart.totalPrice)) {
+      cart.totalPrice = 0;
+    }
+
+    await cart.save();
+
+    await Wishlist.findByIdAndDelete(productId);
+
+    return res.status(200).json(cart);
+  } catch (err) {
+    console.error("Error in moveToCart:", err);
+    return res.status(500).send("Internal server error");
+  }
+};
+
+module.exports = { addToWishlist, getWishlist, moveToCart };
