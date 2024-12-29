194b3fcd4f7595965f2381d8242beabab4cdb520
Muhammed Fathah <muhammedfathahofficial@gmail.com>
2024-12-29 17:10:28 +0530

    added the atlas link in the database connection

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/scentmagic');
+    const conn = await mongoose.connect(process.env.MONGO_CONNECTION_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
