8e1e891146e8c4572d75bac69b5f219ed63c1d6b
Muhammed Fathah <muhammedfathahofficial@gmail.com>
2025-01-04 22:43:02 +0530

    added the image only upload validation

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,8 +1,8 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs')
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-const uploadFolder = path.join(__dirname, '../public/uploads/products');
+const uploadFolder = path.join(__dirname, "../public/uploads/products");
 
 if (!fs.existsSync(uploadFolder)) {
   fs.mkdirSync(uploadFolder, { recursive: true });
@@ -10,18 +10,29 @@ if (!fs.existsSync(uploadFolder)) {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    
-    cb(null, uploadFolder); 
+    cb(null, uploadFolder);
   },
   filename: (req, file, cb) => {
-   
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
+const fileFilter = (req, file, cb) => {
+  const fileExtension = path.extname(file.originalname).toLowerCase();
+
+  const allowedExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"];
+
+  if (allowedExtensions.includes(fileExtension)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, 
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 module.exports = upload;
